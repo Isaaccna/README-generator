5b8a09a0686731efc485c5648d9a07b76656e392
Isaaccna <isaac.andrade1231@gmail.com>
2021-12-04 18:16:09 -0500

    answers printed successfully

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,9 @@
-// TODO: Include packages needed for this application
+// packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
+// array of questions for user input
 const questions = () => {
     return inquirer.prompt([
         {
@@ -61,11 +62,23 @@ const questions = () => {
     ])
 };
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  questions()
+  .then((answers) => {
+    console.log(answers);
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+ 
+}
 
 // Function call to initialize app
 init();

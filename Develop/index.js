8e0305f26dbeff42d4bb923773a80ec5f55471d9
Isaaccna <isaac.andrade1231@gmail.com>
2021-12-04 17:54:51 -0500

    inquerer questions added

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,65 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What's the title of your project?"
+        },
+
+        {
+            type: "input",
+            name: "description",
+            message: "Describe your project"
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Installation intructions"
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Usage Information:",
+          },
+          {
+            type: "input",
+            name: "contributors",
+            message: "Contibutors to the project:",
+          },
+          {
+            type: "input",
+            name: "tests",
+            message: "Tests for the project:",
+          },
+          {
+            type: "input",
+            name: "github",
+            message: "What is your GitHub username?",
+          },
+          {
+            type: "input",
+            name: "email",
+            message: "What is your email address?",
+          },
+          {
+            type: "checkbox",
+            name: "license",
+            choices: [
+              "MIT",
+              "GNU GPLv3",
+              "GNU AGPLv3",
+              "GNU LGPLv3",
+              "Apache License 2.0",
+            ],
+          },
+
+    ])
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
